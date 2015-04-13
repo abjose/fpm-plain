cf35acea9c0be1acc86d9727225af6f632c8d5b9
abjose <ajose@caltech.edu>
2015-04-12 20:11:04 -0700

    add simple panning and zooming

--- InputManager.ts
+++ InputManager.ts
@@ -4,32 +4,31 @@ module fpm {
 
     selected;
     pressed;
+    //x_offset: number; y_offset: number;
+    init_drag_x: number; init_drag_y: number;
+    init_vx: number; init_vy: number;
+    panning;
+
     
     constructor() {
       var self = this;
       this.selected = {};
       this.pressed = {};
-
-      // $(function(){
-      // 	$(document).mousemove(function(e){
-      // 	  if (e.target.type != 'textarea') {
-      // 	    var mouse_x = e.pageX;
-      // 	    var mouse_y = e.pageY;
-      // 	    new fpm.TextBox({
-      // 	      id: "node"+Math.random(),
-      // 	      x: mouse_x, y: mouse_y, w: 150, h: 150,
-      // 	      text: "New textbox!",
-      // 	    });
-      // 	  }
-      // 	});
-      // });
+      this.init_drag_x = 0; this.init_drag_y = 0;
+      this.init_vx = 0; this.init_vy = 0;
+      this.panning = false;
 
       $(function(){
 	$(document).mousedown(function(e){
 	  self.pressed['mouse'] = true;
+	  self.init_drag_x = e.pageX;
+	  self.init_drag_y = e.pageY;
+	  self.init_vx = viewrect.vx;
+	  self.init_vy = viewrect.vy;
 	  
 	  if (e.target.type != 'textarea') {
 	    self.clear_selection();
+	    self.panning = true;
 	  }
 
 	  var ids = Object.keys(self.selected);
@@ -52,6 +51,7 @@ module fpm {
       $(function(){
 	$(document).mouseup(function(e){
 	  delete self.pressed['mouse'];
+	  self.panning = false;
 	  // how can you this won't run before textbox version (meaning
 	  // textbox wouldn't highlight)?
 	  gd.update(graph);
@@ -62,11 +62,21 @@ module fpm {
 	$(document).mousemove(function(e){
 	  if (self.is_pressed('mouse')) {
 	    var mouse_x = e.pageX; var mouse_y = e.pageY;
-	    var ids = Object.keys(self.selected);
-	    for (var i = 0; i < ids.length; ++i) {
-	      var node = self.selected[ids[i]];
-	      if (node.editing) continue;
-	      node.move_to(mouse_x - node.x_offset, mouse_y - node.y_offset);
+	    // TODO: would prefer not to use this.panning
+	    if (e.target.type == 'textarea' && !self.panning) {
+	      // weird, even when you set to != text area, still drags a bit...
+	      var selected_ids = self.get_selected();
+	      for (var i = 0; i < selected_ids.length; ++i) {
+		var node = self.selected[selected_ids[i]];
+		if (node.editing) continue;
+		node.move_to(mouse_x - node.x_offset, mouse_y - node.y_offset);
+	      }
+	    } else {
+	      var scale = viewrect.get_scale();
+	      var world_dx = (e.pageX - self.init_drag_x) / scale.x;
+	      var world_dy = (e.pageY - self.init_drag_y) / scale.y;
+	      viewrect.move_to(self.init_vx - world_dx,
+			       self.init_vy - world_dy);
 	    }
 	    gd.update(graph);
 	  }
--- ViewRect.ts
+++ ViewRect.ts
@@ -10,6 +10,7 @@ module fpm {
       this.rx = 0; this.ry = 0;
       this.rw = 500; this.rh = 500;
       var view = this;
+
       $(function(){
 	$(document).keydown(function(e){
 	  //console.log(e.keyCode);
@@ -26,33 +27,72 @@ module fpm {
 	  case 40: // down
 	    view.vy += 10;
 	    break;
-	  case 173: // -
-	    view.zoom_center(0.9);
-	    break;
-	  case 61:  // +
-	    view.zoom_center(1.1);
-	    break;
+	  // case 173: // -
+	  //   view.zoom_center(0.9);
+	  //   break;
+	  // case 61:  // +
+	  //   view.zoom_center(1.1);
+	  //   break;
 	  }
-	  //n1.draw();
-	  //n2.draw();
-	  //n3.draw();
 	  gd.update(graph);
 	});
       });
+
+      $(function(){
+	$(document).on('wheel', function(e){
+	  if (e.originalEvent.deltaY < 0) {
+	    view.zoom_center(0.9);
+	  } else {
+	    view.zoom_center(1.1);
+	  }
+	  gd.update(graph);
+	});
+      });
+
+
+      // hmm, just add move_to to viewrect? or translate
+      // need to store original click position in inputhandler
+      // so maybe just do most of this in inputhandler
+      
+      // $(function(){
+      // 	$(document).mousemove(function(e){
+      // 	  if (e.target.type != 'textarea' && im.is_pressed('mouse')) {
+	    
+      // 	    var mouse_x = e.pageX;
+      // 	    var mouse_y = e.pageY;
+      // 	    var pt = viewrect.screen_to_world(e.pageX, e.pageY);
+      // 	    var w = 150; var h = 150;
+      // 	    graph.add_node(new fpm.TextBox({
+      // 	      id: "node"+Math.random(),
+      // 	      x: pt.x - w/2, y: pt.y - h/2, w: w, h: h,
+      // 	      text: "New textbox!",
+      // 	    }));
+      // 	    gd.update(graph);
+      // 	  }
+      // 	});
+      // });
+
+      
     }
 
+    move_to(x: number, y: number) {
+      this.vx = x; this.vy = y;
+    }
+    
     world_to_screen(x: number, y: number) {
       var scale = this.get_scale();
       return {x: (x - this.vx) * scale.x,
 	      y: (y - this.vy) * scale.y,
 	      sx: scale.x, sy: scale.y};
     }
+    
     screen_to_world(x: number, y: number) {
       var scale = this.get_scale();
       return {x: x / scale.x + this.vx,
 	      y: y / scale.y + this.vy,
 	      sx: 1 / scale.x, sy: 1 / scale.y};
     }
+    
     //scale_at_point(x: number, y: number, scale: number) {}
     zoom_center(scale_factor: number) {
       var center = this.get_view_center();
@@ -62,9 +102,12 @@ module fpm {
       this.vy = center.y - this.vh / 2;
       //this.y = 2 * this.y / this.h + 1 - scale_factor;
     }
+    
     get_view_center() {
       return {x: this.vx + this.vw / 2, y: this.vy + this.vh / 2};
     }
+
+    // consider adding two versions, for screen->world and vice-versa
     get_scale() {  // should be called like "scale_to_render" or something
       return {x: this.rw/this.vw, y: this.rh/this.vh};
     }
