b275d903bad7501b71bd6049d4b8fe6dabc9666a
abjose <ajose@caltech.edu>
2015-04-25 23:15:54 -0700

    add simple graph stack

--- User.ts
+++ User.ts
@@ -1,12 +1,12 @@
 module fpm {
   export class User {
-    current_graph_id;
+    graph_stack: string[];  // stack of graph ids
     stored_views;  // [id] -> view_param
     view: ViewRect;
-
+    
     constructor() {
       this.stored_views = {};
-      this.current_graph_id = "";
+      this.graph_stack = [];
       this.view = new ViewRect(this);
     }
 
@@ -15,11 +15,18 @@ module fpm {
 	this.stored_views[graph.id] = graph.default_view.copy();
       }
       this.view.set_view(this.stored_views[graph.id]);
-      this.current_graph_id = graph.id;
+      // TODO: 'pop back' to ID if already in garph_stack
+      this.graph_stack.push(graph.id);
     }
 
     get_graph() {
-      return registry.get(this.current_graph_id);
+      return registry.get(this.graph_stack[this.graph_stack.length-1]);
+    }
+
+    pop_graph() {
+      if (this.graph_stack.length > 0) {
+	this.graph_stack.pop();
+      }
     }
   }
 }
